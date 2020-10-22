e84195ffeeba6bb3f9168e4cfe72db514140a779
Shubham Singhal <shubham.011190@gmail.com>
2020-10-22 15:50:36 +0530

    Post model and routes

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyparser = require('body-parser');
+require('dotenv/config');
+app.use(bodyparser.json());
+
+const postRoutes = require('./routes/posts');
+app.use('/posts', postRoutes);
+
+
+app.get('/', (req, res) => {
+    res.send("We are at homepage!");
+})
+
+
+
+mongoose.connect(process.env.DB_CONN,{ useNewUrlParser: true }, () => {
+    console.log("Connected to DB");
+})
+
+
+app.listen(3000, () => {
+    console.log("Server started");
+})
---
+++ routes/posts.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const Post = require('../models/Post');
+
+router.get('/', (req, res) => {
+    res.send("Posts!");
+});
+router.post('/', (req, res) => {
+    console.log(req.body);
+})
+router.get('/specific', (req, res) => {
+    res.send("specific post");
+});
+module.exports = router;
