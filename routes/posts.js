ce940a2276e273b526c120409ecce16ea6c3a748
Shubham Singhal <shubham.011190@gmail.com>
2020-10-22 16:27:19 +0530

    Update

--- routes/posts.js
+++ routes/posts.js
@@ -6,9 +6,19 @@ router.get('/', (req, res) => {
     res.send("Posts!");
 });
 router.post('/', (req, res) => {
-    console.log(req.body);
-})
-router.get('/specific', (req, res) => {
-    res.send("specific post");
+    const post = new Post({
+        title: req.body.title,
+        description: req.body.description,
+    });
+    post.save()
+        .then(data => {
+            res.status(200).json(data);
+            console.log(data);
+        })
+        .catch(err => {
+            res.status(400).json({message : err});
+        });
+    
 });
+
 module.exports = router;
